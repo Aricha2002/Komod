e4256ad27113f4ebbf1b2cd8105b64e9289026de
PhValerie <PhValerie@yandex.ru>
2023-02-17 23:43:34 +0300

    new

--- Komod/SwiftUIView.swift
+++ Komod/SwiftUIView.swift
@@ -11,6 +11,8 @@ struct SwiftUIView: View {
     var body: some View {
          
         
+       let button = UIButton()
+        
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
